800ff4472a6f2610130dddb96754755c13d60a3a
vsenko <vsenko@gmail.com>
2023-03-03 15:17:59 +0000

    Возможность выбора файла в примере

--- docs/js/vue-app.js
+++ docs/js/vue-app.js
@@ -5,6 +5,7 @@ new Vue({ // eslint-disable-line no-new, no-undef
     description: 'Тестовое подписание',
     name: 'Блок данных на подпись',
     dataB64: 'MTEK',
+    dataFile: '',
     isPDF: false,
     attachData: false,
     qrCodeImage: null,
@@ -18,8 +19,19 @@ new Vue({ // eslint-disable-line no-new, no-undef
       this.waiting = true;
 
       try {
+        let dataB64ToSend = this.dataB64;
+        if (this.dataFile) {
+          dataB64ToSend = await (new Promise((resolve, _) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.readAsDataURL(this.$refs.fileUploadInput.files[0]);
+          }));
+
+          dataB64ToSend = dataB64ToSend.split(',')[1];
+        }
+
         const qrSigner = new QRSigningClientCMS(this.description, this.attachData);
-        qrSigner.addDataToSign([this.name], this.dataB64, [], this.isPDF);
+        qrSigner.addDataToSign([this.name], dataB64ToSend, [], this.isPDF);
         const qrCode = await qrSigner.registerQRSinging();
         this.qrCodeImage = `data:image/gif;base64,${qrCode}`;
         [this.signature] = await qrSigner.getSignatures(() => {
