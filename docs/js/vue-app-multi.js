4ef06fb4b7dfc64e1b9467129a0850c09a5af773
vsenko <vsenko@gmail.com>
2023-06-06 12:54:38 +0000

    Пример подписания нескольких документов за одну операцию

---
+++ docs/js/vue-app-multi.js
@@ -0,0 +1,60 @@
+new Vue({ // eslint-disable-line no-new, no-undef
+  el: '#app',
+
+  data: {
+    description: 'Тестовое подписание',
+    name: 'Блок данных на подпись',
+    attachData: false,
+    dataFiles: [],
+    qrCodeImage: null,
+    eGovMobileLaunchLink: '',
+    eGovBusinessLaunchLink: '',
+    signatures: [],
+    waiting: false,
+  },
+
+  methods: {
+    addDataFile() {
+      this.dataFiles.push('');
+    },
+
+    async sign() {
+      this.waiting = true;
+
+      try {
+        const qrSigner = new QRSigningClientCMS(this.description, this.attachData);
+
+        for(const fileUploadInput of this.$refs.fileUploadInputs) {
+          let dataB64ToSend = await (new Promise((resolve, _) => {
+            const reader = new FileReader();
+            reader.onloadend = () => resolve(reader.result);
+            reader.readAsDataURL(fileUploadInput.files[0]);
+          }));
+
+          qrSigner.addDataToSign([fileUploadInput.files[0].name], dataB64ToSend.split(',')[1]);
+        }
+
+        const qrCode = await qrSigner.registerQRSinging();
+        this.qrCodeImage = `data:image/gif;base64,${qrCode}`;
+        this.eGovMobileLaunchLink = qrSigner.getEGovMobileLaunchLink();
+        this.eGovBusinessLaunchLink = qrSigner.getEGovBusinessLaunchLink();
+        const signatures = await qrSigner.getSignatures(() => {
+          this.qrCodeImage = null;
+        });
+
+        for (const signature of signatures) {
+          this.signatures.push(signature);
+        }
+
+        this.waiting = false;
+      } catch (err) {
+        this.waiting = false;
+        alert(`${err}: ${err.details}`);
+      }
+    },
+  },
+
+  mounted() {
+    this.addDataFile();
+  },
+});
